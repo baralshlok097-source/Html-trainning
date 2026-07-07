559081b8c7388f915529a2eddcb07e1ca324d2a5
baralshlok097-source <baralshlok097@gmail.com>
2026-07-07 13:36:16 +0545

    Add files via upload

---
+++ Day11/Java.js
@@ -0,0 +1,49 @@
+ function greet(name) {
+     console.log("Hello" + name)
+ }
+
+ greet("Alice");
+
+ function add(a, b) {
+     return a + b;
+ }
+
+ let result = add(5, 3);
+ console.log(result);
+
+ function greet() {
+     console.log("HI"+name);
+ }
+ greet("Shlok Baral");
+
+function add(a, b) {
+     return a + b;
+ }
+ let sum = add(33, 44);
+ console.log(sum);
+
+
+ function greet() {
+     console.log("Hello");
+ }
+greet("Figma");
+
+function rename(a, b){
+    console.log(a-b);
+}
+ let sub = subtract(2, 3);
+ console.log(subtract);
+
+ function greet(name){
+    console.log("Hello"+name);
+ }
+ greet("Gang");
+
+ function many(){
+    console.log(a*b);
+    
+ }
+
+let multi= multiply(22, 2);
+ console.log(multi);
+
---
+++ Day11/Tranning.js
@@ -0,0 +1,35 @@
+let name = "Shlok";
+let age = 21;
+let X = 22;
+let Y = 222;
+
+console.log(X + Y);
+console.log(X - Y);
+console.log(X & Y);
+console.log(X * Y);
+console.log(X % Y);
+console.log(name);
+console.log(age);
+
+function greet(name) {
+    console.log("Hello" + name)
+}
+
+greet("Alice");
+
+function add(a, b) {
+    return a + b;
+}
+
+let result = add(5, 3);
+console.log(result);
+
+function greet() {
+    console.log("HI");
+}
+greet("Shlok Baral");
+function add(a, b) {
+    return a + b;
+}
+let sum = add(33, 44);
+console.log(sum);
